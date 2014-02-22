5cc3df8a3fdc6ae5cfe47cd7129607f40b7a2bd2
Cameron Carroll <ckcarroll4@gmail.com>
2014-02-21 20:06:12 -0800

    Add tagger module which updates mp3 with title and artist.

---
+++ lib/tagger.rb
@@ -0,0 +1,16 @@
+require 'taglib'
+
+module Tagger
+  def self.tag(filename, artist, title)
+    puts title
+    if File.exists? filename
+      TagLib::MPEG::File.open(filename) do |file|
+        tag = file.tag()
+        tag.artist = artist
+        tag.title = title
+        file.save
+      end
+    end
+  end
+
+end
